b078408ef7a22964e950907437730c250de9d035
Manuel Jesus Donoso Perez <dev@manueldonoso.es>
2024-05-17 01:24:37 +0200

    pantalla lista

--- app/src/main/java/com/tarea7/examen/pantallaListado.java
+++ app/src/main/java/com/tarea7/examen/pantallaListado.java
@@ -1,6 +1,8 @@
 package com.tarea7.examen;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class pantallaListado extends AppCompatActivity {
 
+    private Button btn_volver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,18 @@ public class pantallaListado extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btn_volver=findViewById(R.id.btn_volver);
+        listenerbutton();
+    }
+
+    public void listenerbutton(){
+        btn_volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
